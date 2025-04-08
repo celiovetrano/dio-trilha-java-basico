f81f5ddf928714c3756c315aba880ac36d2934eb
celiovetrano <celio.vetrano@gmail.com>
2025-04-08 14:10:27 -0300

    Gerando Docs

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.vetrano.application"},{"l":"com.vetrano.entities"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"}];updateSearchResults();
--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.vetrano.entities","l":"SmartTv"},{"p":"com.vetrano.application","l":"Usuario"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ContaTerminal"}];updateSearchResults();
